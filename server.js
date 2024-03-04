d5953540e482580947b8419d98c79ad074d1eb75
monineathsar <monineathsar@gmail.com>
2024-03-03 21:40:01 -0800

    Added static path for js files

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const expsesh = require('express-session');
 const cors = require('cors')
 
 const SequelizeStore = require('connect-session-sequelize')(expsesh.Store);
-
 const sequelize = require('./config/connection');
 
 // routes global variables
@@ -16,7 +15,6 @@ const routes = require('./controllers/homepageController');
 const jobCardRoute = require('./controllers/jobCardController');
 
 // handlebars init
-
 const hbs = exphbs.create({});
 const viewsPath = path.join(__dirname, './views');
 hbs.handlebars.registerHelper('dateFormat', function (dateTime) {
@@ -58,14 +56,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // handlebars template engine
-
 app.engine('handlebars', hbs.engine);
 app.set('views', viewsPath);
 app.set('view engine', 'handlebars');
 
-
 // middlewares
-
 app.use(cors());
 app.use(express.static('public'));
 
@@ -73,13 +68,19 @@ app.use(expsesh(sessionSettings));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public', {
+    setHeaders: (res, path, stat) => {
+        if (path.endsWith('.js')) {
+            res.type('application/javascript');
+        }
+    },
+}));
 
 // jobform page (home), and jobCard page
 app.use(routes);
 app.use(jobCardRoute);
 
 // server listener + sequelize sync
-
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Go to http://localhost:3001/'));
 });
